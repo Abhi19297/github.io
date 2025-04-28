af7e77cd6637bbe4c9af4082c3df3b170056b203
Abhijeet Thakur <thakurabhi19297@gmail.com>
2025-04-28 11:17:53 +0000

    feat: fix base url

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,30 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'picsum.photos',
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //   ],
+  // },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    unoptimized: true, // Disable image optimization for static export
   },
+  basePath: '/github.io',
+  assetPrefix: '/github.io/',
+  
 };
 
+// module.exports = nextConfig;
 export default nextConfig;
